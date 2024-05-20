124538ed1db0fcbd60cedb22f7b3c8827e2c6dff
ahdev <53409839+ahslcdev@users.noreply.github.com>
2024-05-19 21:21:50 -0300

    feat: novos filtros para get nos pedidos

--- apps/core/api/filters.py
+++ apps/core/api/filters.py
@@ -1,10 +1,16 @@
-from django_filters import FilterSet, NumberFilter
+from django_filters import FilterSet, NumberFilter, DateFilter
 
 from apps.core.models import Pedido
 
 class PedidoFilters(FilterSet):
-    id_user = NumberFilter(field_name='id_user', lookup_expr='exact')
-    
+    id_user = NumberFilter(field_name='id_user', lookup_expr='exact', label='ID do usuário', )
+    data_inicio = DateFilter(field_name='criado_em__date', lookup_expr='gte', label='Data inicial da criação do pedido')
+    data_fim = DateFilter(field_name='criado_em__date', lookup_expr='lte', label='Data final da criação do pedido')
+
     class Meta:
-        fields = ('id_user',)
+        fields = [
+            'id_user', 
+            'data_inicio', 
+            'data_fim',
+        ]
         model = Pedido
